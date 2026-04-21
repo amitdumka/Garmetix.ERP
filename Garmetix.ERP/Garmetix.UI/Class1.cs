bc41764d51ff5aa81f1c6b3870cb2989d7ba83f8
Amit Kumar <amitsha.2407@gmail.com>
2026-04-21 21:47:39 +0530

    ui lib

---
+++ Garmetix.ERP/Garmetix.UI/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Garmetix.UI
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ Garmetix.ERP/Garmetix.UI/Platforms/MacCatalyst/PlatformClass1.cs
@@ -0,0 +1,7 @@
+﻿namespace Garmetix.UI
+{
+    // All the code in this file is only included on Mac Catalyst.
+    public class PlatformClass1
+    {
+    }
+}
