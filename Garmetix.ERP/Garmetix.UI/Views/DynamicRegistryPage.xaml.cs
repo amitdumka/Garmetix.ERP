b50d0179f7ca729799c594a5ee3e6475bd7ca0a2
Amit Kumar <amitsha.2407@gmail.com>
2026-04-22 15:45:19 +0530

    okay

--- Garmetix.ERP/Garmetix.UI/Views/DynamicRegistryPage.xaml.cs
+++ Garmetix.ERP/Garmetix.UI/Views/DynamicRegistryPage.xaml.cs
@@ -5,6 +5,29 @@ namespace Garmetix.UI.Views
 {
     public partial class DynamicRegistryPage : ContentPage
     {
+
+        // Add this property to catch the string from AppShell.xaml
+        public string EntityType
+        {
+            get => (BindingContext as DynamicRegistryViewModel)?.EntityType;
+            set
+            {
+                if (BindingContext is DynamicRegistryViewModel vm)
+                {
+                    vm.EntityType = value; // Pass it straight to the ViewModel!
+                }
+            }
+        }
+
+        // 1. ADD THIS: XAML needs this empty constructor to build the UI from AppShell
+        public DynamicRegistryPage()
+        {
+            InitializeComponent();
+
+            // Safely grab the ViewModel from MAUI's Dependency Injection system
+            var serviceProvider = IPlatformApplication.Current.Services;
+            BindingContext = serviceProvider.GetService<DynamicRegistryViewModel>();
+        }
         public DynamicRegistryPage(DynamicRegistryViewModel viewModel)
         {
             InitializeComponent();
